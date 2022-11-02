602c1566d71f91feec48c1fe2847d5c744516b3e
Masar <92muma1bwi@hft-stuttgart.de>
2022-11-02 09:50:45 +0100

    Subheader und die Legende implementiert

--- overview/src/pages/Home.tsx
+++ overview/src/pages/Home.tsx
@@ -1,10 +1,13 @@
 import { Stack } from "@mui/material";
 import NavBar from "../components/Navbar";
+import SubHeader from "../components/Subheader";
+
 
 export default function Home() {
   return (
     <Stack>
       <NavBar />
+      <SubHeader />
     </Stack>
   );
 }
