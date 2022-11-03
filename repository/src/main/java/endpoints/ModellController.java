c150d82d6f1be2de42182d528ad18d34c5ab5eaf
Afsin Demir <k2@SlattBook-Pro.local>
2022-11-03 12:58:08 +0100

    Abfragen werden nun mit 'baseUrl/overview/ID' statt wie vorher mit 'baseUrl/Modell/ID'

--- repository/src/main/java/endpoints/ModellController.java
+++ repository/src/main/java/endpoints/ModellController.java
@@ -10,7 +10,7 @@ import java.util.List;
 import java.util.NoSuchElementException;
 
 @RestController
-@RequestMapping("/Modell")
+@RequestMapping("/overview")
 public class ModellController {
     @Autowired
     Service modellService;
