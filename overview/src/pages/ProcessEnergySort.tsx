a7e1e07e74d07ce5e2471513d2a70fcdce530ccf
Natalie Zavarko <natalie.zavarko@gmail.com>
2022-11-13 17:42:27 +0100

    WIP: Energy Sort Changes

---
+++ overview/src/pages/ProcessEnergySort.tsx
@@ -0,0 +1,39 @@
+import { Stack } from "@mui/material";
+import NavBar from "../components/Navbar";
+import { useProcessStore } from "../stores/process_store";
+import ProcessLandscape from "../components/ProcessLandscape";
+import { useParams } from "react-router-dom";
+
+export default function ProcessEnergySort() {
+  const { energySumYear } = useParams();
+
+  const allProcesses: any = useProcessStore((state) => state.allProcesses);
+
+  const filteredProcesses: any = allProcesses.filter((obj) => {
+    return obj.energySumYear <= 30;
+  });
+
+  const parentProcessIndex = allProcesses.findIndex(
+    (p: any) => p.id == id
+  );
+
+  return (
+    <div>
+      <Stack>
+        <NavBar
+          title={"Übersicht: " + allProcesses[parentProcessIndex].name}
+        />
+        <ProcessLandscape
+          parentProcess={allProcesses[parentProcessIndex]}
+          coreProcesses={allProcesses.filter(
+            (p: any) => p.parentProcess === id && p.processType === "core"
+          )}
+          managementProcesses={allProcesses.filter(
+            (p: any) => p.parentProcess === id && p.processType === "management"
+          )}
+          supportProcesses={allProcesses.filter((p: any) => p.parentProcess === id && p.processType === "support")}
+        />
+      </Stack>
+    </div>
+  );
+}
