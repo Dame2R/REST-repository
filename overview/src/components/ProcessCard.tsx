f9c4ff5756a5332bc7c59415ec8c47e6542fd6c3
FreddyStraub <straub.freddy@web.de>
2022-12-06 16:40:25 +0100

    feat: :sparkles: Update processCard

--- overview/src/components/ProcessCard.tsx
+++ overview/src/components/ProcessCard.tsx
@@ -1,72 +1,72 @@
-import { Card, Typography, CardContent, Box, Stack } from '@mui/material';
-import { useNavigate } from 'react-router-dom';
+import { Card, Typography, CardContent, Box, Stack } from "@mui/material";
+import { useNavigate } from "react-router-dom";
 
 type ProcessCardProps = {
-    process: any;
-}
-
-function colorBox(energy: number): string {
-    if (energy <= 30) {
-        return '#33CC00' /* green */
-    }
-    else if (energy > 30 && energy <= 170) {
-        return '#FFEF00' /* yellow */
-    }
-    else if (energy > 170) {
-        return '#FF3300' /* red */
-    };
-    return 'gray'
+  process: any;
 };
 
-export const ProcessCard = (props: ProcessCardProps) => {
-
-    let navigate = useNavigate();
-
-    var energySumYear = props.process.energySumYear;
-    var newBackground: string = colorBox(energySumYear);
-
-    return (
-        <Card
-        onClick={() => {
-            console.log("first")
-            navigate("/details/" + props.process.id);
-        }}
-            sx={{
-                maxWidth: 300,
-                minWidth: 300,
-                margin: "auto",
-                cursor: "pointer",
-                padding: 0,
-                '&:hover': {
-                    boxShadow: "0 16px 70px -12.125px rgba(0,0,0,0.3)"
-                },
-            }}
-        >
-            <CardContent sx={{
-                padding: 0,
-                "&:last-child": {
-                    paddingBottom: 0
-                }
-            }}>
-                <Stack direction='row' justifyContent='space-between'>
-                    <Typography variant='h6' fontWeight='bold' padding={3}>
-                        {props.process.name}
-                    </Typography>
-                    <Box
-                        sx={{
-                            width: '25%',
-                    textAlign: 'center',
-                    display: 'flex',
-                    justifyContent: 'center', /* align horizontal */
-                    alignItems: 'center', /* align vertical */
-                    fontWeight: 'bold',
-                        }} bgcolor={colorBox(energySumYear)}
-                    >
-                    <Typography variant='h6' fontWeight='bold'>{energySumYear}</Typography>
-                </Box>
-            </Stack>
-
-        </CardContent >
-        </Card >
-    );
+function colorBox(energy: number): string {
+  if (energy <= 30) {
+    return "#33CC00"; /* green */
+  } else if (energy > 30 && energy <= 170) {
+    return "#FFEF00"; /* yellow */
+  } else if (energy > 170) {
+    return "#FF3300"; /* red */
+  }
+  return "gray";
 }
+
+export const ProcessCard = (props: ProcessCardProps) => {
+  let navigate = useNavigate();
+
+  var energySumYear = props.process.energySumYear;
+  var newBackground: string = colorBox(energySumYear);
+
+  return (
+    <Card
+      onClick={() => {
+        navigate("/details/" + props.process.id);
+      }}
+      sx={{
+        maxWidth: 700,
+        minWidth: 300,
+        margin: "auto",
+        cursor: "pointer",
+        padding: 0,
+        "&:hover": {
+          boxShadow: "0 16px 70px -12.125px rgba(0,0,0,0.3)",
+        },
+      }}
+    >
+      <CardContent
+        sx={{
+          padding: 0,
+          "&:last-child": {
+            paddingBottom: 0,
+          },
+        }}
+      >
+        <Stack direction="row" justifyContent="space-between">
+          <Typography variant="h6" fontWeight="bold" padding={3}>
+            {props.process.name}
+          </Typography>
+          <Box
+            sx={{
+              width: "25%",
+              textAlign: "center",
+              display: "flex",
+              justifyContent: "center" /* align horizontal */,
+              alignItems: "center" /* align vertical */,
+              fontWeight: "bold",
+            }}
+            bgcolor={colorBox(energySumYear)}
+          >
+            <Typography variant="h6" fontWeight="bold">
+              {energySumYear}
+            </Typography>
+          </Box>
+        </Stack>
+      </CardContent>
+    </Card>
+  );
+};
--- overview/src/pages/ProcessEnergySort.tsx
+++ overview/src/pages/ProcessEnergySort.tsx
@@ -1,8 +1,10 @@
-import { Stack } from "@mui/material";
+import { Grid, Stack, Box } from "@mui/material";
 import NavBar from "../components/Navbar";
 import { useProcessStore } from "../stores/process_store";
 import ProcessLandscape from "../components/ProcessLandscape";
 import { useParams } from "react-router-dom";
+import { ProcessCard } from "../components/ProcessCard";
+import SubHeader from "../components/Subheader";
 
 export default function ProcessEnergySort() {
   const { energyLevel } = useParams();
@@ -18,30 +20,37 @@ export default function ProcessEnergySort() {
   }
   if (energyLevel === "medium") {
     filteredProcesses = allProcesses.filter((obj: any) => {
-      return obj.energySumYear > 30 && obj.energySumYear <= 60;
+      return obj.energySumYear > 30 && obj.energySumYear <= 170;
     });
   }
   if (energyLevel === "high") {
     filteredProcesses = allProcesses.filter((obj: any) => {
-      return obj.energySumYear > 60;
+      return obj.energySumYear > 170;
     });
   }
+
+  filteredProcesses.sort((a: any, b: any) =>
+    a.energySumYear < b.energySumYear ? 1 : -1
+  );
+
+  const processCards = filteredProcesses.map((process: any) => {
+    return (
+      <Grid item>
+        <ProcessCard process={process} />
+      </Grid>
+    );
+  });
 
   return (
     <div>
       <Stack>
         <NavBar />
-        <ProcessLandscape
-          coreProcesses={filteredProcesses.filter(
-            (p: any) => p.processType === "CORE"
-          )}
-          managementProcesses={filteredProcesses.filter(
-            (p: any) => p.processType === "MANAGEMENT"
-          )}
-          supportProcesses={filteredProcesses.filter(
-            (p: any) => p.processType === "Supportprozess"
-          )}
-        />
+        <SubHeader />
+        <Box padding={5}>
+          <Grid container spacing={2}>
+            {processCards}
+          </Grid>
+        </Box>
       </Stack>
     </div>
   );
