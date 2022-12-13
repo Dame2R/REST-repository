81d2cbaacacd278fd97b244e54021f2a4e08ea94
Lukas Preidt <01prlu1bwi@hft-stuttgart.de>
2022-12-13 17:48:39 +0000

    Update repository/src/test/java/repo/SpringbootBackendApplicationTests.java

--- repository/src/test/java/repo/SpringbootBackendApplicationTests.java
+++ repository/src/test/java/repo/SpringbootBackendApplicationTests.java
@@ -3,7 +3,7 @@ package repo;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit5.SpringRunner;
+import org.springframework.test.context.junit4.SpringRunner;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
