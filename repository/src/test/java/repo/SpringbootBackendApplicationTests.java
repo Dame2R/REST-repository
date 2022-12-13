35fdc509a9a7a78fb9119e10b62943adac8276f7
lukaspreidt <lpreidt@gmail.com>
2022-12-13 18:20:56 +0100

    Modified Unit tests

--- repository/src/test/java/repo/SpringbootBackendApplicationTests.java
+++ repository/src/test/java/repo/SpringbootBackendApplicationTests.java
@@ -4,14 +4,57 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.Before;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class SpringbootBackendApplicationTests {
 	
+    @Autowired
+	private WebApplicationContext webApplicationContext;
 
+	private MockMvc mockMvc;
+
+	@Before
+    public void setup() {
+		mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+	}
 	@Test
-	public void contextLoads() {
+	 public void contextLoads() {
+	}
+	@Test
+	public void testProcessOverview() throws Exception {
+        //Expected Output
+        /*    
+    "id": "Process_0vjv4zs",
+    "childProcess": "Process_1kndcr1;Process_1kndcr1;",
+    "startKnoten": "StartEvent_1;Event_1po129z;",
+    "endKnoten": "Event_0jwg8pn;Event_1yedj2w;",
+    "energySumYear": 50,
+    "processType": "nicht core",
+    "department": "Verkauf",
+    "processDescription": "Dies ist eine Prozessbeschreibung\nad"
+} */
+	mockMvc.perform(get("/overview/Process_0vjv4zs")).andExpect(status().isOk())
+				.andExpect(content().contentType("application/json;"))
+                .andExpect(jsonPath("$.id").value("Process_0vjv4zs"))
+                .andExpect(jsonPath("$.childProcess").value("Process_1kndcr1;Process_1kndcr1;"))
+                .andExpect(jsonPath("$.startKnoten").value("StartEvent_1;Event_1po129z;"))
+                .andExpect(jsonPath("$.endKnoten").value("Event_0jwg8pn;Event_1yedj2w;"))
+                .andExpect(jsonPath("$.energySumYear").value(50))
+                .andExpect(jsonPath("$.processType").value("nicht core"))
+                .andExpect(jsonPath("$.department").value("Verkauf"))
+                .andExpect(jsonPath("$.processDescription").value("Dies ist eine Prozessbeschreibung\nad"));
+				
 	}
 
 	
